607dc1c13518b87b7bbb5ecf9a67caf7ca255436
Birendra Chaudhary <birendrachaudhary008@gmail.com>
2025-04-19 16:37:28 +0545

    Learing Day 1

---
+++ Module01/Tutorial01.py
@@ -0,0 +1,23 @@
+#   Understanding Variable and DataTypes
+
+# INteger
+age =25
+print(type(age))
+print("Age: ", age)
+
+#Float
+height=5.11
+print(type(height))
+print("Height: ", height)
+
+#String
+Name="Birendra"
+print(type(Name))
+print("Name: ",Name)
+
+
+#Boolen 
+is_person=True
+print(type(is_person))
+print("Is Birendra a Human",is_person)
+
---
+++ Module01/Tutorial02.py
@@ -0,0 +1,34 @@
+# Working with Numbers , String, Booleans
+
+
+#Numbers
+a=15
+b=5
+
+sum=a+b
+divide=a/b
+mul=a*b
+sub=a-b
+
+print("Sum: ",sum)
+print("Subtract: ",sub)
+print("Multiplication: ",mul)
+print("Division: ",divide)
+
+
+#String
+First_name= "Birendra"
+Last_name="Chaudhary"
+
+Full_name= First_name+" "+Last_name
+print(Full_name)
+
+
+#Boolean 
+sun_raise=True
+sun_set=True
+
+if sun_raise and not sun_set:
+    print("Enjoy your Day Time")
+else:
+    print("Enjoy your Night Time")
