e8a037075de143fd073247d827adfe54ce7d9407
Birendra Chaudhary <birendrachaudhary008@gmail.com>
2025-04-19 17:08:15 +0545

    Basics Syntax of Python

---
+++ Module01/Tutorial03.py
@@ -0,0 +1,14 @@
+#using input() function
+
+name=input("Enter Your Name: ")
+age=input("Enter Your Age: ")
+
+print("Your name is: ",name)
+print("You are " ,age, " years old")
+
+A=int(input("Enter the Value of A: "))
+B=int(input("Enter the Value of B: "))
+
+sum = A+B
+
+print("The Sum of ",+A+" and ",+B+" is ",sum)
