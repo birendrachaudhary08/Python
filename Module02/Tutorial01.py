08ffb6c52ae12cac0106c6abf2e3f966077e90d0
Birendra Chaudhary <birendrachaudhary008@gmail.com>
2025-04-19 17:25:25 +0545

    if, elif, else statement

--- Module02/Tutorial01.py
+++ Module02/Tutorial01.py
@@ -1,1 +1,14 @@
 # IF, Elif, Else condtion
+
+# WAP to find the greatest of 3 numbers entered by the user
+
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+c=int(input("Enter third  number: "))
+
+if a>b and a>c:
+    print("First number is greater")
+elif b>a and b>c:
+        print("Second number is greater")
+else:
+          print("Third number is greater")
