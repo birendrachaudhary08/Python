3d56c4ad09dbefbe80ad1cd92449456b5cfda69e
Birendra Chaudhary <birendrachaudhary008@gmail.com>
2025-04-19 18:04:15 +0545

    Control Flow Statement

--- Module02/Tutorial03.py
+++ Module02/Tutorial03.py
@@ -9,4 +9,10 @@ i=1
 while i<11:
     print(a," x ",i," = ",a*i)
     i=i+1
-print(i)
+
+print()
+
+#do while
+#In Python, there is no construct defined for do while loop. 
+#Python loops only include for loop and while loop but we can modify the while loop to 
+# work as do while as in any other languages such as C++ and Java.
