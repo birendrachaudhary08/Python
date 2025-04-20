cd10f3a14b59df0bdc21e78d0802b1c4febd7bd2
Birendra Chaudhary <birendrachaudhary008@gmail.com>
2025-04-20 16:36:45 +0545

    Data Structure

--- Module04/Tutorial01.py
+++ Module04/Tutorial01.py
@@ -1,4 +1,29 @@
 #Creating and manipulating lists
+
+
+"""
+Lists are used to store multiple items in a single variable.
+Lists are one of 4 built-in data types in Python used to store collections of data, the other 3 are Tuple, Set, and Dictionary, 
+all with different qualities and usage.
+
+Lists are created using square brackets
+List items are ordered, changeable, and allow duplicate values.
+List items are indexed, the first item has index [0], the second item has index [1] etc.
+
+List Method 
+append()	Adds an element at the end of the list
+clear()	Removes all the elements from the list
+copy()	Returns a copy of the list
+count()	Returns the number of elements with the specified value
+extend()	Add the elements of a list (or any iterable), to the end of the current list
+index()	Returns the index of the first element with the specified value
+insert()	Adds an element at the specified position
+pop()	Removes the element at the specified position
+remove()	Removes the item with the specified value
+reverse()	Reverses the order of the list
+sort()	Sorts the list
+
+"""
 cars= ["BMW","THAR","Range Rover", "Audi", "Porsche","Lamborghini"]
 
 #       0       1       2              3        4           5
@@ -17,11 +42,3 @@ cars.remove("BMW")
 print("List after removing a car: ",cars)
 
 
-"""List operation and method
-    1.append : use to add single element
-    2.extend : use to add multiple element
-    3.insert : use to add element with their address  .insert(1,"Element")
-    4.remove : use to delete the element from list
-    4.pop : use to delete the element from list 
-
-"""
